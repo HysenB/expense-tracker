cd3dc0287051dbfeaf98f982ec3f1f438fb1e0c3
HysenB <hysi2000@live.com>
2024-06-14 11:20:51 +0200

    add transcation

--- src/components/AddTransaction.vue
+++ src/components/AddTransaction.vue
@@ -1,3 +1,15 @@
 <template>
-    <h2>Expense Tracker</h2>
+    <h3>Add new transaction</h3>
+    <form id="form">
+        <div class="form-control">
+            <label for="text">Text</label>
+            <input type="text" id="text" placeholder="Enter text..." />
+        </div>
+        <div class="form-control">
+            <label for="amount">Amount <br />
+                (negative - expense, positive - income)</label>
+            <input type="text" id="amount" placeholder="Enter amount..." />
+        </div>
+        <button class="btn">Add transaction</button>
+    </form>
 </template>
